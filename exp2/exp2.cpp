43191202f88999c97c79a0082e5a8903c326c13b
PS <1020612624@qq.com>
2018-06-19 13:21:07 +0800

    Update

--- exp2/exp2.cpp
+++ exp2/exp2.cpp
@@ -2,20 +2,20 @@
  * @Author: B15020411
  * @Date: 2018-06-19 09:59:26
  * @LastEditors: B15020411
- * @LastEditTime: 2018-06-19 12:48:11
+ * @LastEditTime: 2018-06-19 13:21:02
  * @Description: この素晴らしい世界に爆焔を！
  */
 // 二、打字程序（10）
-//     要求：（1）随即产生一字符串，每次产生的字符串内容、长度都不同；
-//          （2）根据（1）的结果，输入字符串，判断输入是否正确，输出正确率；
-//          （3）具有输入输出界面。
+// 要求：
+// （1）随即产生一字符串，每次产生的字符串内容、长度都不同；
+// （2）根据（1）的结果，输入字符串，判断输入是否正确，输出正确率；
+// （3）具有输入输出界面。
 #include <algorithm>
 #include <ctime>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
-// 65~90 97~122
 using namespace std;
 char dic[26 + 26 + 10];
 void rand_str(string& str) {
