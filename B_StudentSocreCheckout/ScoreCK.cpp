064b294e2bfe770b07b540bce77543824deff7e0
PS <1020612624@qq.com>
2018-06-21 17:49:31 +0800

    Update

---
+++ B_StudentSocreCheckout/ScoreCK.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: B15020411
+ * @Date: 2018-06-21 17:48:15
+ * @LastEditors: B15020411
+ * @LastEditTime: 2018-06-21 17:49:07
+ * @Description: この素晴らしい世界に爆焔を！
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+typedef struct info {
+  int NO;
+  string name;
+  string major;
+} student;
